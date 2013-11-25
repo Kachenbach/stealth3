27b439142111fa4d33d751b4ef75c0a31383a6c2
Kachenbach <kachenba@my.hpu.edu>
2013-11-24 22:04:51 -1000

    fdsfdd

---
+++ Assets/Scripts/Alarm Systems/AlarmLight.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+public var fadeSpeed : float = 2f;
+public var highIntensity : float = 2f;
+public var lowIntensity : float = .5f;
+public var changeMargin : float = .2f;
+public var alarmOn : boolean;
+
+
+private var targetIntensity : float;
+
+function awake()
+{
+	light.intensity = 0f;
+	targetIntensity = highIntensity;
+}
+
+function Update () {
+
+	if(alarmOn) {
+		light.intensity = Mathf.Lerp(light.intensity, targetIntensity, fadeSpeed * Time.deltaTime);
+		CheckTargetIntensity();
+	}
+	else
+		light.intensity = Mathf.Lerp(light.intensity, 0f, fadeSpeed * Time.deltaTime);
+	
+
+}
+
+function CheckTargetIntensity () {
+	if(Mathf.Abs(targetIntensity - light.intensity) < changeMargin) {
+		if(targetIntensity == highIntensity)
+			targetIntensity = lowIntensity;
+		else
+			targetIntensity = highIntensity;
+	}
+}
+
---
+++ Assets/Scripts/PlayerController/FirstPersonMove.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+var movementSpeed = 5.0;
+function Start () {
+	
+}
+
+function Update () {
+	var forwardSpeed = Input.GetAxis("Vertical");
+	var sideSpeed = Input.GetAxis("Horizontal");
+	var speed = new Vector3(sideSpeed * movementSpeed * 10 ,0 ,forwardSpeed * movementSpeed);
+	var cc : CharacterController = gameObject.GetComponent(CharacterController);
+	cc.SimpleMove(speed);
+	
+	
+}
